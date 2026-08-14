04c35cc28416a905a9a0cbe30ed7d58b4d45c6a4
khyathig <khyathigutta22@gmail.com>
2026-08-14 12:54:18 +0530

    Exploring pipelines in Jenkins

---
+++ TestFactorial.java
@@ -0,0 +1,9 @@
+public class TestFactorial {
+
+    public static void main(String[] args) {
+        
+               int n = 5;
+
+        System.out.println("Factorial = " + Factorial.fact(n));
+    }
+}
